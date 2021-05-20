1af81802bd6414fc32bec8105d44a09105510fe5
Patryk Adrian Zdziech <01159035@pw.edu.pl>
2021-05-20 16:12:33 +0000

    Upload New File

---
+++ PROI_Projekt4/PROI_Projekt4/Employee.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Name.h"
+#include <string>
+#include "permision.h"
+#include <set>
+#include <iostream>
+
+class Employee
+{
+	Name name;
+	static set<int> availableNumbers;
+	int employeeNumber,stationNumber;
+	set <permision> permissions;
+	set <responsibility> responsibilities;
+public:
+	Employee(std::string firstName, std::string lastName);
+	Name getName()
+	{
+		return this->name;
+	}
+		virtual ~Employee();
+		int getEmployeeNumber() const;
+		void setEmployeeNumber(int empoyeeNumber);
+		void setEmployeeNumber();
+		string getFirstName();
+		string getLastName();
+		int getStationNumber() const;
+		void setStationNumber(int stationNumber);
+		const set<permision>& getPermisionsGranted() const;
+		void addPermision(permision permisionGranted);
+		void removePermision(permision permisionToRemove);
+		void clearPermisions();
+		bool canHaveResposibility(const responsibility resp);
+		bool addResponsibility(responsibility responsibilityAdded);
+		void removeResposibility(responsibility responsibilityToRemove);
+		void clearResponsiblities();
+		const set<responsibility>& getResponsibilities() const;
+		bool operator<(const Employee& r);
+		std::ostream& operator<<(std::ostream& os);
+};
