b41c8b07c853a678252384776ab37347a135e02b
PatrykZdziech <50749577+Renesans123@users.noreply.github.com>
2022-07-02 15:20:18 +0200

    Update main.cpp

--- PROI_Projekt4/PROI_Projekt4/main.cpp
+++ PROI_Projekt4/PROI_Projekt4/main.cpp
@@ -7,15 +7,12 @@ int main(int argc, char* argv[])
 {
     srand((unsigned)time(NULL));
 
+    Bank b = Bank(1, 1);
+    b.simulate("");
+    return 0;
+	
     //                      TESTS 
-
     //int result = Catch::Session().run(argc, argv);
     //return result;
-
     //                  END TESTS
-
-	//Bank b = Bank(1, 1);
-	//b.simulate();
-    runSimulation(7, 77);
-    return 0;
 }
